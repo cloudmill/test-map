64770dda4b740bf974eb2d6613c061ae4cb9716c
frxzenCurse <nikitaableev@mail.ru>
2023-04-07 16:59:29 +0300

    update

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import "./styles/app.scss";
+
+import "./scripts/swiper";
+import "./scripts/select2";
+import "./scripts/map";
---
+++ src/scripts/map.js
@@ -0,0 +1,100 @@
+
+function isTouchDevice() {
+  return (('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const pictures = document.querySelectorAll('[data-pin-image]')
+  let isTouch = isTouchDevice()
+
+  pictures.forEach(picture => {
+    const img = picture.querySelector('img')
+    const container = img.closest('[data-pin-container]')
+    const offset = {
+      top: img.offsetTop,
+      left: img.offsetLeft
+    }
+    let isImageLoaded;
+    let imageWidth;
+    let imageHeight;
+    let isDragging
+    let mousePos = { x: null, y: null }
+
+    const imageLoaded = () => {
+      imageWidth = img.offsetWidth
+      imageHeight = img.offsetHeight
+      isImageLoaded = true
+    }
+
+    if (img.complete) {
+      imageLoaded()
+    } else {
+      img.addEventListener('load', () => {
+        imageLoaded()
+      })
+    }
+
+    const onMouseDown = (event) => {
+      if (isImageLoaded) {
+        isDragging = true
+        mousePos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
+      }
+    }
+
+    const onMouseMove = (event) => {
+      if (isDragging) {
+        const currentMousePos = { x: event.pageX - offset.left, y: event.pageY - offset.top }
+        const changeX = currentMousePos.x - mousePos.x;
+        const changeY = currentMousePos.y - mousePos.y;
+
+        mousePos = currentMousePos
+
+        var imgTop = parseInt($(picture).css('top'), 10);
+        var imgLeft = parseInt($(picture).css('left'), 10);
+        
+        var imgTopNew = imgTop + changeY;
+        var imgLeftNew = imgLeft + changeX;
+
+        if(imgTopNew > 0)
+          imgTopNew = 0;
+        if(imgTopNew < (container.offsetHeight - imageHeight))
+          imgTopNew = container.offsetHeight - imageHeight;
+
+        if(imgLeftNew > 0)
+        imgLeftNew = 0;
+        if(imgLeftNew < (container.offsetWidth - imageWidth))
+        imgLeftNew = container.offsetWidth - imageWidth;
+
+        $(picture).css({ top: imgTopNew + 'px', left: imgLeftNew + 'px' });
+      }
+    }
+    
+    if (isTouch) {
+      container.addEventListener('touchstart', (event) => {
+        onMouseDown(event)
+      })
+
+      window.addEventListener('touchend', () => {
+        isDragging = false
+      })
+  
+      container.addEventListener('touchmove', (event) => {
+        onMouseMove(event)
+      })
+    } else {
+      container.addEventListener('mousedown', (event) => {
+        onMouseDown(event)
+      })
+
+      window.addEventListener('mouseup', () => {
+        isDragging = false
+      })
+  
+      container.addEventListener('mousemove', (event) => {
+        onMouseMove(event)
+      })
+    }
+  })
+})
